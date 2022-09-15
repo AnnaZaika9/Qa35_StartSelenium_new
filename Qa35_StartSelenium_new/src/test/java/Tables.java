a0b3aea6b77033622b62bd3f3920f4d172cc9fdb
AnnaZaika9 <a.horujaya@gmail.com>
2022-09-15 11:14:08 +0300

    HomeWork (css -> xPath; xPath->css)

--- Qa35_StartSelenium_new/src/test/java/Tables.java
+++ Qa35_StartSelenium_new/src/test/java/Tables.java
@@ -56,25 +56,41 @@ public class Tables {
         // print count rows in table
         List<WebElement> listRows = wd.findElements(By.cssSelector("#country-table tr"));
         List<WebElement> xListRows = wd.findElements(By.xpath("//*[@id='country-table']//tr"));
-        System.out.println("Count of rows = " +xListRows.size());
-        Assert.assertEquals(xListRows.size(),4);
+        List<WebElement> rows = wd.findElements(By.xpath("//tr")); // razbor
+        List<WebElement> rows1 = wd.findElements(By.cssSelector("tr")); //css
+
+        wd.findElement(By.xpath("table[@id='country-table']/tbody/tr")); /// == 4
+        wd.findElement(By.cssSelector("table#country-table>tbody>tr"));
+
+        wd.findElement(By.xpath("table[@id='country-table']//tr")); // == 4
+        wd.findElement(By.cssSelector("table#country-table tr"));
+
+        System.out.println("Count of rows = " +rows.size());
+        Assert.assertEquals(rows.size(),4);
 
         // print last rows
         WebElement lastRows = wd.findElement(By.cssSelector("#country-table tr:last-child"));
         WebElement xLastRows = wd.findElement(By.xpath("//*[@id='country-table']//tr[last()]"));
-        System.out.println("Last rows: " +xLastRows.getText());
+        WebElement lastrow = wd.findElement(By.xpath("//tr[last()]")); //razbor
+        wd.findElement(By.cssSelector("tr:last-child"));
+        System.out.println("Last rows: " +lastrow.getText());
+        Assert.assertEquals(lastrow.getText(),"Poland Chine Mexico");
 
         // print count of columns
         List<WebElement> listColumns = wd.findElements(By.cssSelector("#country-table tr:first-child td"));
         List<WebElement> xListColumns = wd.findElements(By.xpath("//*[@id='country-table']//tr[1]/td"));
-        System.out.println("Count of columns = " + xListColumns.size());
-        Assert.assertEquals(xListColumns.size(),3);
+        List<WebElement> listcol = wd.findElements(By.xpath("//tr[1]/td")); //razbor
+        wd.findElement(By.cssSelector("tr:first-child>td"));
+        System.out.println("Count of columns = " + listcol.size());
+        Assert.assertEquals(listcol.size(),3);
 
         // print text Israel
         WebElement israel = wd.findElement(By.cssSelector("#country-table tr:nth-child(2) td:nth-child(2)"));
         WebElement xIsrael = wd.findElement(By.xpath("//*[@id='country-table']//tr[2]/td[2]"));
-        System.out.println("Text: " + xIsrael.getText());
-        Assert.assertEquals(xIsrael.getText(),"Israel");
+        WebElement is = wd.findElement(By.xpath("//tr[2]/td[2]")); // razbor
+        wd.findElement(By.cssSelector("tr:nht-child(2):nht-child(2)"));
+        System.out.println("Text: " + is.getText());
+        Assert.assertEquals(is.getText(),"Israel");
 
 
         wd.close();
